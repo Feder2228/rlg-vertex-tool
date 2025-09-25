7dfbabb3e112c93ae6f1a66b48ec69c4c4ea7dbd
Feder2228 <107298134+Feder2228@users.noreply.github.com>
2025-09-25 19:07:15 +0200

    add ge command

--- rlg-vertex-tool.py
+++ rlg-vertex-tool.py
@@ -29,11 +29,19 @@ PROMPT_STR_HELP = '''
     For each file it finds the .dae of, it reads the vertices of the .dae and overwrites the .rlg's vertices with those.
     (Saves the modified .rlg as a copy in the "output" folder. The original .rlg won't be modified)
               
+    x
+    exit
+
+    help
+    show this message
+    
     
     DEV COMMANDS:
 
-    eobjs 
-    [disabled in current version, will be re-added in the future] Same as "e" command, but each group (mesh) is saved in a different .obj file (for dev purposes only. Those objs can't be used to recreate an .rlg file)
+    ge
+    Does the g command thing, then for each .rlg file it creates, it exports it as .dae again.
+    You'll find both an .rlg and its .dae equivalent in the output folder.
+    Nice QoL feature for testing.
 
     d
     Create a .txt file containing various data about each group (mesh) of the .rlg file. The .txt will be saved in the "output" folder.
@@ -47,41 +55,26 @@ def __main__():
         r = input("\n\n" +PROMPT_STR_BASE_COMMANDS+ "\n\n")
 
         # Check if response is exit
-        if(r == "x"):
+        if( r == "x" ):
             exit()
 
         rlg_filenames = util.get_all_filenames_of_specified_extension( DIR_PATH_INPUT_NLG_FORMATS, ".rlg" )
         dae_filenames = util.get_all_filenames_of_specified_extension( DIR_PATH_INPUT_COMMON_FORMATS, ".dae" )
 
         # check response and execute if it's a valid command
-        if(r == "d"):
-            for rlgname in rlg_filenames:
-                rlgpath = DIR_PATH_INPUT_NLG_FORMATS + rlgname
-                print_misc_data_to_file( rlgname, rlg.read_rlg( rlgpath ) )
+        if( r == "d" ):
+            print_misc_data_to_file( rlg_filenames )
 
-        elif(r == "e"):
-            for rlgname in rlg_filenames:
-                rlgpath = DIR_PATH_INPUT_NLG_FORMATS + rlgname
-                daename = rlgname + ".dae"
-                daepath = DIR_PATH_OUTPUT + daename
-                dae.create_dae( rlg.read_rlg( rlgpath ), daepath )
+        elif( r == "e" ):
+            export_rlg_as_dae( rlg_filenames )
 
-        elif(r == "g"):
-            for rlgname in rlg_filenames:
-                daename = rlgname + ".dae"
+        elif( r == "g" ):
+            generate_rlg_from_rlg_and_dae( rlg_filenames, dae_filenames )
 
-                if daename not in dae_filenames:
-                    print( rlgname + ".dae not found" )
-                    continue
-                print( "found " + daename )
-
-                src_rlg_path = DIR_PATH_INPUT_NLG_FORMATS + rlgname
-                dst_rlg_path = DIR_PATH_OUTPUT + rlgname
-                daepath = DIR_PATH_INPUT_COMMON_FORMATS + daename
-
-                rlg.patch_rlg( src_rlg_path, dst_rlg_path, dae.read_dae( daepath ) )
+        elif( r == "ge" ):
+            generate_rlg_from_rlg_and_dae( rlg_filenames, dae_filenames, auto_export=True )
         
-        elif(r == "help"):
+        elif( r == "help" ):
             print( PROMPT_STR_HELP + "\n")
 
         else:
@@ -90,17 +83,55 @@ def __main__():
         input("Press Enter to continue...")
 
 
+def export_rlg_as_dae( rlg_filenames ):
+    for rlgname in rlg_filenames:
+        rlgpath = DIR_PATH_INPUT_NLG_FORMATS + rlgname
+        daename = rlgname + ".dae"
+        daepath = DIR_PATH_OUTPUT + daename
+        dae.create_dae( rlg.read_rlg( rlgpath ), daepath )
 
 
-# function that prints data to file
-def print_misc_data_to_file( filename, model3d ):
+def generate_rlg_from_rlg_and_dae( rlg_filenames, dae_filenames, auto_export=False ):
+    for rlgname in rlg_filenames:
+        daename = rlgname + ".dae"
 
-    txtfile = open( DIR_PATH_OUTPUT + filename + "_miscdata.txt", "w" )
+        if daename not in dae_filenames:
+            print( rlgname + ".dae not found" )
+            continue
+        print( "found " + daename )
 
-    txtfile.write( str( model3d ) )
+        src_rlg_path = DIR_PATH_INPUT_NLG_FORMATS + rlgname
+        dst_rlg_path = DIR_PATH_OUTPUT + rlgname
+        daepath = DIR_PATH_INPUT_COMMON_FORMATS + daename
 
-    txtfile.close()
-    print( filename + "_miscdata.txt file successfully created in output folder" )
+        rlg.patch_rlg( src_rlg_path, dst_rlg_path, dae.read_dae( daepath ) )
+        print( 'created rlg file at {0}'.format( dst_rlg_path ) )
+
+        # automatic exportation: automatically export the newly generated rlg to dae.
+        # This is QoL for testing.
+        # In the output folder you won't only find your new .rlg, but also the .dae
+        # equivalent of that .rlg, ready to be imported in blender and be checked for
+        # inaccuracies.
+        #
+        if auto_export:
+            auto_export_dae_path = DIR_PATH_OUTPUT + daename
+            dae.create_dae( rlg.read_rlg( dst_rlg_path ), auto_export_dae_path )
+            print( 'created dae file at {0}'.format( auto_export_dae_path ) )
+
+
+
+def print_misc_data_to_file( rlg_filenames ):
+
+    for rlgname in rlg_filenames:
+
+        rlgpath = DIR_PATH_INPUT_NLG_FORMATS + rlgname
+        model = rlg.read_rlg( rlgpath )
+
+        txtfile = open( DIR_PATH_OUTPUT + rlgname + "_miscdata.txt", "w" )
+        txtfile.write( str( model ) )
+        txtfile.close()
+
+        print( rlgname + "_miscdata.txt file successfully created in output folder" )
 
 
 
