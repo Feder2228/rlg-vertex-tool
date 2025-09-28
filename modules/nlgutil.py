10933a19116dfc9d68eb7e6c32e152c349f0431b
Feder2228 <107298134+Feder2228@users.noreply.github.com>
2025-09-28 08:08:14 +0200

    move get_hash_name to nlgutil.py

--- modules/nlgutil.py
+++ modules/nlgutil.py
@@ -1,8 +1,11 @@
-from modules import util
-
 # N.L.G. Utilities 
 # This file is for functions that are related to decoding/encoding NLG file
 # formats and are useful to more than one module (i.e. both rlg.py and shier.py)
+
+from modules import util
+
+STRING_SECTION_START = 0x1C8F4  # this is for get_hash_name
+
 
 class Section:
     def __init__( self, header_location, size, flags ):
@@ -82,9 +85,53 @@ def get_map_of_sections( file ):
             file.seek( 1, 1 )  # move forward by 1 until you're aligned
 
         
-
-    
     return section_map
             
 
 
+
+
+# function that maps an hash to its name. Requires hashid.bin path to work.
+# binpath: filepath (str); hash: hash (integer)
+# return string containing the name associated to the hash
+#
+def get_hash_name( binpath, hash ):
+
+    binfile = open( binpath, 'rb' )
+
+
+    # look for the hash, start from the beginning of the file
+    binfile.seek( 0, 0 )
+
+    string_offset = -1
+
+    # loop until the hash section ends
+    while binfile.tell() < STRING_SECTION_START:
+
+        binfile.read(4)  # skip 4
+        scanned_hash = binfile.read(4)
+
+        if scanned_hash == hash.to_bytes( 4, 'big' ):
+            string_offset = int.from_bytes( binfile.read(4), 'big' )
+            break
+    
+
+    # if you couldn't find the hash, return None
+    if string_offset == -1:
+        return None
+    
+
+    # go to where the string is
+    binfile.seek( STRING_SECTION_START + string_offset , 0)
+        
+    hash_string = ''    
+
+    # read the bytes
+    while True:
+
+        next_byte = binfile.read(1)
+
+        if next_byte == b'\x00':
+            return hash_string
+        
+        hash_string += next_byte.decode( 'ascii' ) 
